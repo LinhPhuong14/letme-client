39068d532b2184d2b8cde17e103589797ca75b61
Linh Phuong Nguyen <167509270+LinhPhuong14@users.noreply.github.com>
2025-04-13 01:03:12 +0700

    Add files via upload

---
+++ server/utils.py
@@ -0,0 +1,17 @@
+import parselmouth
+from transformers import pipeline
+from math import sqrt
+import matplotlib.pyplot as plt
+import cv2
+import os
+import requests
+import pandas as pd
+import numpy as np
+import sounddevice as sd
+import wave
+import threading
+import time
+from pydub import AudioSegment
+import subprocess
+import mediapipe as mp
+from moviepy import VideoFileClip, AudioFileClip
